bec64dffb0c2a6005deb97dfd972b4e7ac920b6a
jamil2gomes <jamil.lannister23@gmail.com>
2019-12-16 15:22:34 -0300

    refatoração no teste de integracao do pagamento

--- TesteIntegracao/src/test/java/com/br/es2/repository/PagamentoRepositoryTeste.java
+++ TesteIntegracao/src/test/java/com/br/es2/repository/PagamentoRepositoryTeste.java
@@ -1,11 +1,8 @@
 package com.br.es2.repository;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import java.time.LocalDate;
-import java.util.List;
-
-import org.mockito.Mockito;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -17,6 +14,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.br.es2.builder.EmprestimoBuilder;
 import com.br.es2.builder.LivroBuilder;
 import com.br.es2.builder.UsuarioBuilder;
 
@@ -24,19 +22,15 @@ import br.com.es2.model.Emprestimo;
 import br.com.es2.model.Livro;
 import br.com.es2.model.Pagamento;
 import br.com.es2.model.Usuario;
-import br.com.es2.repository.EmprestimoRepository;
-import br.com.es2.repository.EmprestimoRepositoryImplementacao;
 import br.com.es2.repository.PagamentoRepository;
 import br.com.es2.repository.PagamentoRepositoryImplementacao;
-import br.com.es2.service.EmprestimoService;
 
 public class PagamentoRepositoryTeste {
 	private EntityManager manager;
 	private static EntityManagerFactory emf;
-	
+
 	private Livro livro;
 	private Usuario usuario;
-	private EmprestimoRepository emprestimoRepo;
 	private PagamentoRepository pagamentoRepo;
 
 	@BeforeAll
@@ -48,8 +42,6 @@ public class PagamentoRepositoryTeste {
 	public void antes() {
 		manager = emf.createEntityManager();
 		manager.getTransaction().begin();
-		emprestimoRepo = Mockito.mock(EmprestimoRepositoryImplementacao.class);
-		
 		pagamentoRepo = new PagamentoRepositoryImplementacao(manager);
 		livro = LivroBuilder.umLivro().comAutor("Autor X").comTitulo("Titulo Y").constroi();
 		usuario = UsuarioBuilder.umUsuario().comMatricula("123").comNome("Jamil").constroi();
@@ -65,36 +57,24 @@ public class PagamentoRepositoryTeste {
 	public static void fim() {
 		emf.close();
 	}
-	
 
 	@Test
 	public void deveSalvarPagamento() {
-		
-		EmprestimoService servico = new EmprestimoService();
-		servico.setRepositorio(emprestimoRepo);
-		
-		List<Emprestimo>emprestimosRealizado = servico.realizarEmprestimo(usuario, livro);
-		
-		
-		
-		
-		double valorAPagar = servico.realizaDevolucao(emprestimosRealizado.get(0), LocalDate.now());
-		
-		Pagamento pagamento  = new Pagamento(valorAPagar, 
-				emprestimosRealizado.get(0).getUsuario(),
-				emprestimosRealizado.get(0));
-		
+
+		Emprestimo emprestimo = EmprestimoBuilder.umEmprestimo().comUsuario(usuario).comLivro(livro).constroi();
+
+		Pagamento pagamento = new Pagamento(5.0, emprestimo.getUsuario(), emprestimo);
+
 		pagamentoRepo.salva(pagamento);
-		
+
 		Pagamento pagamentoBuscado = pagamentoRepo.buscaPor(1L);
-		
-		
-		Mockito.verify(emprestimoRepo, Mockito.times(1)).atualiza(emprestimosRealizado.get(0));
-		
-		assertNotNull(pagamentoBuscado);
-		
-		
-		
+
+		assertAll(  () -> assertNotNull(pagamentoBuscado),
+					() -> assertEquals(1L, pagamentoBuscado.getId()),
+					() -> assertEquals(1L, pagamentoBuscado.getUsuario().getId()),
+					() -> assertEquals(1L, pagamentoBuscado.getEmprestimo().getId())
+				);
+
 	}
-	
+
 }
